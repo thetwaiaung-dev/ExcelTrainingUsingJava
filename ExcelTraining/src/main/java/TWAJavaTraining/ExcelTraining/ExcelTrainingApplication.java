be0eccb08a0cfc42668f401502d192600d0ce191
Thet Wai Aung <126937793+thetwaiAung-Developer@users.noreply.github.com>
2024-05-23 16:41:18 +0630

    Excel Report Java Project Structure

---
+++ ExcelTraining/src/main/java/TWAJavaTraining/ExcelTraining/ExcelTrainingApplication.java
@@ -0,0 +1,13 @@
+package TWAJavaTraining.ExcelTraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelTrainingApplication.class, args);
+	}
+
+}
